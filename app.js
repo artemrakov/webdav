d86f791340a6d972df8f5494ff5ae334fc3d06a5
Artem Rakov <arakov@amazon.com>
2023-04-18 14:50:35 -0400

    r

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ const express = require('express');
 
 const server = new webdav.WebDAVServer();
 const app = express();
-const port = 3000
+const port = 3000;
 
 // Mount the WebDAVServer instance
 app.use(webdav.extensions.express('/webdav', server));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
-app.listen(3000, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(3000, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
 
-// app.listen(3000); // Start the Express server
+app.listen(3000); // Start the Express server
 
 // const webdav = require('webdav-server').v2;
 //
