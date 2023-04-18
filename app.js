d989cb9a654ab6450083952607d99c3cd978786c
Artem Rakov <arakov@amazon.com>
2023-04-18 15:47:12 -0400

    s

--- app.js
+++ app.js
@@ -1,47 +1,55 @@
-const webdav = require('webdav-server').v2;
-const express = require('express');
+const webdav = require("webdav-server").v2;
+const express = require("express");
 
 // const server = new webdav.WebDAVServer();
 // const app = express();
 
-
 class FileNameFilterPrivilegeManager extends webdav.PrivilegeManager {
-  constructor(allowedFileNames) {
-    super();
-    this.allowedFileNames = allowedFileNames;
-  }
-
-  _can(path, user, callback) {
-    const fileName = path.fileName();
-    if (this.allowedFileNames.includes(fileName)) {
-      callback(null, true);
-    } else {
-      callback(null, false);
+    constructor(allowedFileNames) {
+        super();
+        this.allowedFileNames = allowedFileNames;
     }
-  }
+
+    _checkRights(ctx, path, user, rights, callback) {
+        const fileName = path.fileName();
+        if (this.allowedFileNames.includes(fileName)) {
+            super._checkRights(ctx, path, user, rights, callback);
+        } else {
+            callback(null, false);
+        }
+    }
 }
 
 const server = new webdav.WebDAVServer({
     port: 3000,
-    privilegeManager: new FileNameFilterPrivilegeManager(['vtt']),
+    privilegeManager: new FileNameFilterPrivilegeManager(["vtt"]),
 });
-
 
 server.afterRequest((arg, next) => {
     // Display the method, the URI, the returned status code and the returned message
-    console.log('>>', arg.request.method, arg.requested.uri, '>', arg.response.statusCode, arg.response.statusMessage);
+    console.log(
+        ">>",
+        arg.request.method,
+        arg.requested.uri,
+        ">",
+        arg.response.statusCode,
+        arg.response.statusMessage
+    );
     // If available, display the body of the response
     console.log(arg.responseBody);
     next();
 });
 
-server.setFileSystem('/webdav', new webdav.PhysicalFileSystem('/home/ubuntu/output'), (success) => {
-
-    server.start((s) => {
-        const port = s.address().port;
-        console.log('Ready on port', port);
-    });
-})
+server.setFileSystem(
+    "/webdav",
+    new webdav.PhysicalFileSystem("/home/ubuntu/output"),
+    (success) => {
+        server.start((s) => {
+            const port = s.address().port;
+            console.log("Ready on port", port);
+        });
+    }
+);
 
 // Mount the WebDAVServer instance
 // app.use(webdav.extensions.express('/webdav', server));
