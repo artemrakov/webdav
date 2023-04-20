2af35978072693e12921a721ccf03262bc64fc0e
Artem Rakov <arakov@amazon.com>
2023-04-20 13:31:12 -0400

    tst

--- app.js
+++ app.js
@@ -25,17 +25,23 @@ const server = new webdav.WebDAVServer({
     // privilegeManager: new FileNameFilterPrivilegeManager("vtt"),
 });
 
+server.beforeRequest((arg, next) => {
+    console.log("ARG:", arg);
+
+    next()
+})
+
 server.afterRequest((arg, next) => {
     // Display the method, the URI, the returned status code and the returned message
-    console.log("ARG:", arg);
-    // console.log(
-    //     ">>",
-    //     arg.request.method,
-    //     arg.requested.uri,
-    //     ">",
-    //     arg.response.statusCode,
-    //     arg.response.statusMessage
-    // );
+    // console.log("ARG:", arg);
+    console.log(
+        ">>",
+        arg.request.method,
+        arg.requested.uri,
+        ">",
+        arg.response.statusCode,
+        arg.response.statusMessage
+    );
     // If available, display the body of the response
     console.log(arg.responseBody);
     next();
